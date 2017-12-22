3be193c58057e4af3d55d8c9c767f44dc1a8a373
Rus Hughes <russell.hughes@gmail.com>
2017-12-22 21:41:37 +0700

    base

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+require('./models/User');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+  console.log(Date.now(), req.originalUrl);
+  next();
+});
+
+require('./routes/authRoutes')(app);
+
+if (process.env.NODE_ENV == 'production') {
+  // express will serve up static production assets
+  // main.js main.css etc
+  app.use(express.static('client/build'));
+
+  // express will serve up the index.html if it does
+  // not recognise the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
